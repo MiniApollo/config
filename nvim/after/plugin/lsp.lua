39a9ad1e6c8dbb4ffbaf792064d5dab354371f8c
SMark5 <surmannmark@gmail.com>
2023-02-18 11:50:21 +0100

    renamed sumneko_lua to lua_ls

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -8,12 +8,12 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'sumneko_lua',
+  'lua_ls',
   'rust_analyzer',
 })
 
 -- Fix Undefined global 'vim'
-lsp.configure('sumneko_lua', {
+lsp.configure('lua_ls', {
     settings = {
         Lua = {
             diagnostics = {
