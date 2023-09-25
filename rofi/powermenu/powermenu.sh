ebea9ace1628f4d6be00133e5cbda467bab293e9
SMark5 <surmannmark@gmail.com>
2023-09-25 21:21:12 +0200

    Fixed typo and made user name upporcase

--- rofi/powermenu/powermenu.sh
+++ rofi/powermenu/powermenu.sh
@@ -20,37 +20,37 @@ logout=''
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -dmenu \
-		-p "Visszlát ${USER}" \
-		-mesg "Üzemidő: $uptime" \
-		-theme ${dir}/${theme}.rasi
+    rofi -dmenu \
+	 -p "Viszlát ${USER^}" \
+	 -mesg "Üzemidő: $uptime" \
+	 -theme ${dir}/${theme}.rasi
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$shutdown\n$reboot\n$lock\n$suspend\n$logout" | rofi_cmd
+    echo -e "$shutdown\n$reboot\n$lock\n$suspend\n$logout" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
-		if [[ $1 == '--shutdown' ]]; then
-			loginctl poweroff
-		elif [[ $1 == '--reboot' ]]; then
-			loginctl reboot
-		elif [[ $1 == '--lock' ]]; then
+    selected="$(confirm_exit)"
+    if [[ "$selected" == "$yes" ]]; then
+	if [[ $1 == '--shutdown' ]]; then
+	    loginctl poweroff
+	elif [[ $1 == '--reboot' ]]; then
+	    loginctl reboot
+	elif [[ $1 == '--lock' ]]; then
             swaylock 
-		elif [[ $1 == '--suspend' ]]; then
-			loginctl suspend
-		elif [[ $1 == '--logout' ]]; then
-			if [[ "$XDG_CURRENT_DESKTOP" == 'Hyprland' ]]; then
+	elif [[ $1 == '--suspend' ]]; then
+	    loginctl suspend
+	elif [[ $1 == '--logout' ]]; then
+	    if [[ "$XDG_CURRENT_DESKTOP" == 'Hyprland' ]]; then
                 hyprctl dispatch exit
-			fi
-		fi
-	else
-		exit 0
+	    fi
 	fi
+    else
+	exit 0
+    fi
 }
 
 
@@ -58,18 +58,18 @@ run_cmd() {
 chosen="$(run_rofi)"
 case ${chosen} in
     $shutdown)
-		run_cmd --shutdown
-        ;;
+	run_cmd --shutdown
+	;;
     $reboot)
-		run_cmd --reboot
-        ;;
+	run_cmd --reboot
+	;;
     $lock)
-		run_cmd --lock
-        ;;
+	run_cmd --lock
+	;;
     $suspend)
-		run_cmd --suspend
-        ;;
+	run_cmd --suspend
+	;;
     $logout)
-		run_cmd --logout
-        ;;
+	run_cmd --logout
+	;;
 esac
