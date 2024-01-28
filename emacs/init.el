8fa5dc98a035497e28e6563fe61e43929c20396b
MiniApollo <surmannmark@gmail.com>
2024-01-28 14:08:00 +0100

    Emacs: Remove dashboard

--- emacs/init.el
+++ emacs/init.el
@@ -170,22 +170,6 @@
         doom-modeline-persp-name t   ;; Adds perspective name to modeline
         doom-modeline-persp-icon t)) ;; Adds folder icon next to persp name
 
-(use-package dashboard
-  :init
-  (setq initial-buffer-choice 'dashboard-open)
-  (setq dashboard-set-heading-icons t)
-  (setq dashboard-set-file-icons t)
-  (setq dashboard-banner-logo-title "Emacs Is More Than A Text Editor!")
-  (setq dashboard-center-content nil) ;; set to 't' for centered content
-  (setq dashboard-items '((recents . 5)
-                          (bookmarks . 3)
-                          (projects . 3)
-                          (registers . 3)))
-  :custom
-  (dashboard-modify-heading-icons '((recents . "file-text")
-                                    (bookmarks . "book")))
-  :config
-  (dashboard-setup-startup-hook))
 (use-package projectile
   :config
   (projectile-mode)
