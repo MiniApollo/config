78d985b36070900bd2b50a15f766e87de29071a9
MiniApollo <surmannmark@gmail.com>
2024-02-10 14:15:32 +0100

    Remove neovim config

--- nvim/after/plugin/telescope.lua
+++
@@ -1,5 +0,0 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>pr', builtin.buffers, {})
--- nvim/lua/mark/init.lua
+++
@@ -1,3 +0,0 @@
-require("mark.set")
-require("mark.remap")
-require("mark.lazy")
