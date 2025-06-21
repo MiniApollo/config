8edee14edb2aa8569471155d249f60eb79e4a0c0
MiniApollo <surmannmark@gmail.com>
2025-06-21 17:01:26 +0200

    Delete Qtile config

--- qtile/hybrid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# credit to NinaPirate on reddit
-# https://github.com/AbdelrhmanNile/nvidia-rofi-launcher
-
-prime-run rofi -display-drun "Run on NVIDIA card: " -show drun 
-
-#prime-run $(dmenu_path | dmenu -i -fn 'Monospace' -nb '#013220')
-
