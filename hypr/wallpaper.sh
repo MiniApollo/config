10c10fa2488fb0b7c7d7ac4af6463d6b8e277a3a
Mark Surmann <surmannmark@gmail.com>
2026-02-04 01:04:11 +0100

    Hyprland: Wallpaper Exlude .git, readme

--- hypr/wallpaper.sh
+++ hypr/wallpaper.sh
@@ -3,6 +3,6 @@
 killall swaybg
 
 WALLPAPER_DIRECTORY="$HOME/Pictures/Wallpapers/"
-WALLPAPER=$(find "$WALLPAPER_DIRECTORY" -type f | shuf -n 1)
+WALLPAPER=$(find "$WALLPAPER_DIRECTORY" -path "*/.git" -prune -o -type f ! -name "README.md" -print | shuf -n 1)
 
 swaybg -o \* -i ${WALLPAPER} -m fill &
