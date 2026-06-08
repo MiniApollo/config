8cebe8b47dc4340195aa8a22bfcc39f9c38f01e2
Mark Surmann <surmannmark@gmail.com>
2026-06-08 17:09:19 +0200

    Move scripts to a seperate directory

--- hypr/scripts/wallpaper.sh
+++ hypr/scripts/wallpaper.sh
@@ -5,4 +5,4 @@ killall swaybg
 WALLPAPER_DIRECTORY="$HOME/Pictures/Wallpapers/"
 WALLPAPER=$(find "$WALLPAPER_DIRECTORY" -path "*/.git" -prune -o -type f ! -name "README.md" -print | shuf -n 1)
 
-swaybg -o \* -i ${WALLPAPER} -m fill &
+swaybg -o \* -i ${WALLPAPER} -m fit &
