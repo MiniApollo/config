7c837fdbc8bd0fa4557daeb59c8cedf6c3c6e4b0
Mark Surmann <surmannmark@gmail.com>
2026-07-11 18:34:31 +0200

    Nvim: Use enter for completion

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -78,7 +78,7 @@ return { -- Autocompletion
 				"fallback",
 			},
 
-			preset = "super-tab",
+			preset = "enter",
 
 			-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
 			--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
