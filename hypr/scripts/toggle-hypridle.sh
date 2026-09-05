7a5b0d47a82a62d083cb33a42a31ded2e1e328cb
Mark Surmann <surmannmark@gmail.com>
2026-09-06 00:31:44 +0200

    Hyprland: Change hypridle notification message

--- hypr/scripts/toggle-hypridle.sh
+++ hypr/scripts/toggle-hypridle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if pgrep -x "hypridle" >/dev/null ;then
-  notify-send "Hypridle killed"
+  notify-send "Hypridle turned off"
   killall hypridle
 else
   notify-send "Hypridle started"
